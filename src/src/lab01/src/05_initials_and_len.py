5f9dc00e6559fa0bd042cffa537ad6789cc5c142
Maria <mariaguseva202@gmail.com>
2025-09-13 19:27:04 +0300

    aaaaa

---
+++ src/src/lab01/src/05_initials_and_len.py
@@ -0,0 +1,7 @@
+
+full_name = input("ФИО: ")
+words = full_name.strip().split()
+initials = ''.join(word[0].upper() for word in words if word)
+length = len(full_name.strip())
+print(f"Инициалы: {initials}.")
+print(f"Длина (символов): {length}")
